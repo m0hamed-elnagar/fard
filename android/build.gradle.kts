e7a663d189a43e2cc7af86b1d045da49a7ac9289
NAGAR <mmm30001230\@gmail.com>
2026-02-26 17:19:53 +0200

    fix: revert JVM target to 17 to resolve plugin compatibility issues

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -26,13 +26,13 @@ subprojects {
 
 subprojects {
     tasks.withType<JavaCompile>().configureEach {
-        sourceCompatibility = JavaVersion.VERSION_21.toString()
-        targetCompatibility = JavaVersion.VERSION_21.toString()
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
     }
     
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 }
